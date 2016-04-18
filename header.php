2dc7c128dbbe31aaae082bc27e39bd16e0ebfa60
Alice Guan <aliguan726@gmail.com>
2016-04-18 18:12:10 -0400

    style navigation and make responsive

--- header.php
+++ header.php
@@ -2,17 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Web Interactvitiy and Engagement</title>
     <?php wp_head(); ?>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href=" <?php  bloginfo('stylesheet_url');?>" />
 </head>
 <body>
     <!-- Add Menu Here -->
     <div class="row">
         <div class="three columns logo">
-            <img src="<?php bloginfo('template_directory'); ?>/img/logo.png" width="50%"/>
+            <img src="<?php bloginfo('template_directory'); ?>/img/logo.png" width="20%"/>
         </div>
-        <div class="twelve columns">
+        <div class="nine columns">
             <?php wp_nav_menu(array(
                 'menu' => 'Menu',
                 'sort_column' => 'menu_order',
@@ -26,9 +30,6 @@
                 <h1><a href="<?php bloginfo('url')?>"><?php bloginfo('name');?></a>
                 </h1>
                 <h2><?php bloginfo('description');?></h2>
-            </div>
-            <div class="three columns">
-                 <?php get_search_form(); ?>
             </div>
         </header>
 
