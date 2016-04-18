ce79f2d02a19794d896cdd888136a0340b16a305
Alice Guan <aliguan726@gmail.com>
2016-04-18 18:43:59 -0400

    add background car frontpg image

--- header.php
+++ header.php
@@ -20,13 +20,13 @@
             <?php wp_nav_menu(array(
                 'menu' => 'Menu',
                 'sort_column' => 'menu_order',
-                'container_class' => 'blank-menu-header'
+                'container_class' => 'slicks-menu-header'
             ));?>
         </div>
     </div>
     <div class="container">
         <header class="row">
-            <div class="nine columns">
+            <div class="twelve columns oldtowncar">
                 <h1><a href="<?php bloginfo('url')?>"><?php bloginfo('name');?></a>
                 </h1>
                 <h2><?php bloginfo('description');?></h2>
