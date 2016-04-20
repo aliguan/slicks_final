3a8d680fcc817ca4d175861ad0ca34292ab70e2f
Alice Guan <aliguan726@gmail.com>
2016-04-19 23:05:25 -0400

    add more functionality to blog page

--- blog_custom_page.php
+++ blog_custom_page.php
@@ -7,11 +7,17 @@ Template Name: Blog Page
     <div class="row">
         <div class="twelve columns">
             <?php if (have_posts()) :
-                while (have_posts()) : the_post(); ?>
-                    <h2><?php the_title(); ?></h2>
-                    <?php the_content();
-                endwhile;
-            endif; ?>
+               while (have_posts()) : the_post(); ?>
+                   <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+                   <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                   <?php the_excerpt(__('Continue reading »','example'));
+               endwhile; ?>
+                <!-- Navigation -->
+           <div class="navigation">
+               <span class="newer"><?php previous_posts_link(__('« Newer','example')) ?></span> <span class="older"><?php next_posts_link(__('Older »','example')) ?></span>
+           </div>
+            <?php endif;
+            wp_reset_query(); ?>
         </div>
     </div>
 <?php get_footer(); ?>
