01d815dffca1a1dffa592d0aae7b94681541939e
Alice Guan <aliguan726@gmail.com>
2016-04-21 21:35:32 -0700

    section off blog page to pic and post

--- blog_custom_page.php
+++ blog_custom_page.php
@@ -4,19 +4,18 @@ Template Name: Blog Page
 
 <?php get_header(); ?>
 <?php query_posts('post_type=post&post_status=publish&posts_per_page=5&paged='. get_query_var('paged')); ?>
-    <div class="row">
+    <div class="row" style="padding: 2em;">
         <div class="twelve columns">
             <h1>Slick's Blog</h1>
             <?php if (have_posts()) :
                while (have_posts()) : the_post(); ?>
-               <div class="four columns excerptpic">
+               <div class="three columns excerptpic">
                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
                    <?php the_post_thumbnail(); ?>
                    </a>
                </div>
                <div class="eight columns excerpt list_post">
-                   <h2><?php the_title(); ?></h2>
-                   <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"></a><?php the_excerpt(); ?>
+                  <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"> <h2><?php the_title(); ?></h2></a><?php the_excerpt(); ?>
                </div>
            <?php endwhile; ?>
                 <!-- Navigation -->
